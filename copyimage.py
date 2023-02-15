a405468214dcfb80c9301d0c46e8d3f00248c8c5
zhuanshan <wanghao100@126.com>
2023-02-15 11:20:10 +0800

    第四次

--- copyimage.py
+++
@@ -1,6 +0,0 @@
-import os
-import re
-import urllib
-from multiprocessing import Process
-SUPPORTED_FORMATS=['jpg','png','JPEG']
-URL_TEMPLATE=r'https://visualstudio.microsoft.com/zh-hans/thank-you-downloading-visual-studio/?sku=Community&rel=15#'
