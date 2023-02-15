fd2a3f19708410e23bfc463484ea6e9a091bc02b
zhuanshan <wanghao100@126.com>
2023-02-15 11:12:58 +0800

    第二次提交

--- ex11.py
+++
@@ -1,8 +0,0 @@
-print("How old are you?",end='...')#使输出输入保持在同一行
-age=input()
-print("how tall are you?", end=' ')
-height=input()
-print("how much do you weight?",end=' ')
-weight=input()
-
-print(f"So, you are {age} old, {height} tall and {weight} heavy.")
