0bacd3052bdc1cc0d47c9baae3eac60081d7cc74
zhuanshan <wanghao100@126.com>
2023-02-16 11:41:00 +0800

    2/16

---
+++ chapter6/test.py
@@ -0,0 +1,21 @@
+# 6-1
+
+human = {
+    'last':'zhou',
+    'first': 'wei',
+    'age': '25',
+    'city': 'BJ',
+}
+print(human)
+
+# 6-2
+
+like_numbers = {
+    'a':'yellow',
+    'b':'red',
+    'c':'black',
+}
+print(like_numbers)
+
+# 6-3
+
