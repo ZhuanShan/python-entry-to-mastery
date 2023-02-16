f568d601b1a446c0fbb0df7217b3b91ee022d3a9
zhuanshan <wanghao100@126.com>
2023-02-16 09:53:07 +0800

    2/16

--- text3.py
+++ text3.py
@@ -7,8 +7,9 @@ message = "my first bickcle was a" + bicycles[0].title()
 print(message)
  """
 
-motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles)
+""" motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles) """
+
 """ motorcycles[0] = 'dukadi'
 print(motorcycles) """
 """ motorcycles.append('dukadi')
@@ -73,10 +74,73 @@ else:
     print("sorry") """
 
 
-requested_topping = ['mushrooms','green peppers', 'extra cheese']
+""" requested_topping = ['mushrooms','green peppers', 'extra cheese']
 for requst in requested_topping:
     if requst == 'green peppers':
         print("sorry,")
     else:
         print("adding " + requst)
 print("\nfinish")
+ """
+
+""" available_toppings = ['mushroom', 'olive','green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushroom', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("adding  "+ requested_topping)
+    else:
+        print("sorry")
+print("\nfinish") """
+
+
+""" alien_0 = {'color':'green','points':5} """
+
+""" print(alien_0['color'])
+print(alien_0['points']) """
+
+""" new_points = alien_0['points']
+print("you just "+ str(new_points)+ " points") """
+
+""" alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+alien_0['color'] = 'yellow'
+print(alien_0)
+ """
+
+
+""" alien_0 = {'x_position': 0, 'y_position': 25, 'speed':'medium'}
+print("original x_position:" + str(alien_0['x_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("new x-position:" + str(alien_0['x_position'])) """
+
+
+""" favorite_languages = {
+    'jen':'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+print("sarah's favorite language is "+ 
+    favorite_languages['sarah'].title()+
+    "."
+) """
+
+
+user_0 = {
+    'username':'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user_0.items():
+    print("\nkey: " + key)
+    print("\nvalue:" + value)
