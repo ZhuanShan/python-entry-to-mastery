ac1438b24c668edfec59d7af02cdfaee88e76c2a
zhuanshan <wanghao100@126.com>
2023-03-07 14:58:32 +0800

    3/7

---
+++ train_a_net.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import BinaryCrossentropy
+
+model = Sequential([
+    Dense(units=25, activation='sigmoid')
+    Dense(units=15, activation='sigmoid')
+    Dense(units=1, activation='sigmoid')
+])
+model.compile(loss = BinaryCrossentropy)
+model.fit(X,Y,epochs=100)
